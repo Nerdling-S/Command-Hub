5a1c78c2c93b4ce02c94581896549440c585d593
Nerdling-S <nerdlings9@gmail.com>
2019-11-24 16:15:19 +0200

    Implemented core question-response loop

---
+++ src/hub.cpp
@@ -0,0 +1,40 @@
+#include "libs/lang.hpp"
+#include <string>
+#include <ncurses.h>
+#include <vector>
+
+std::string get_input() {
+    std::string input;
+    nocbreak();
+    echo();
+    int ch = getch();
+    while (ch != '\n') {
+        input += ch;
+        ch = getch();
+    }
+    cbreak();
+    return input;
+}
+
+int main(int argc, const char** argv) {
+    std::vector<Word> thesaurus = initWords("../thesaurus.data");
+    initscr();
+    cbreak();
+    while (true) {
+        clear();
+        printw("Greetings, what would you like to do?");
+        std::string input = get_input();
+        std::vector<int> inputCodes = procInput(input, thesaurus);
+        for (auto it = inputCodes.begin(); it != inputCodes.end(); ++it) {
+            switch (*it)
+            {
+            case QUIT:
+                exit(EXIT_SUCCESS);
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
+}
